41e2ff6e38d057bd69498957902e6d1a2f3fc00a
Alex Urquiza <alex.urz20@gmail.com>
2026-07-31 00:07:10 -0300

    Si no hay tareas aparece un agregar tarea como item, caso contrario aparecen solo las tareas

--- script/agregarTarea.js
+++ script/agregarTarea.js
@@ -1,14 +1,43 @@
 /* ############################# AGREGAR TAREA ############################# */
 /* ######################################################################### */
 
+/* ########################## ITEM AGREGAR TAREA ########################### */
+// Si no existen tareas, entonces se muestra el boton agregar tarea como un item
+
+const verificarSiHayTareas = () => {
+    const contenedorLista = document.querySelector('#taskListItem');
+    const listaTareas = document.querySelectorAll('.taskItem');
+    const emptyTask = document.querySelector('.emptyTask');
+
+    if(listaTareas.length === 0 && !emptyTask){
+        const li = document.createElement('li');
+        li.className = 'emptyTask';
+        li.innerHTML = `
+            <div class="dataTaskItem">
+                <button class="btn-openAddTask" data-btn-event="addTask">
+                    <h2 class="titleh2">Agregar tarea</h2>
+                    <span class="material-symbols-outlined iconoAddTask">add_circle</span>
+                </button>
+            </div>
+        `;
+        contenedorLista.appendChild(li);
+    } else {
+        emptyTask.remove();
+    }
+}
+
+
+
 /* ############################# APERTURA/CIERRE ############################# */
 /* ############################# APERTURA */
-const btn_openAddTask = document.querySelector('#btn-openAddTask');
+// const btn_openAddTask = document.querySelector('.btn-openAddTask');
 const overlay_modalAddTask = document.querySelector('#overlays');
 const modalAddTask = document.querySelector('.modalAddTask');
 
-btn_openAddTask.addEventListener('click', () => {
-    alternarAperturaCierre();
+document.addEventListener('click', (e) => {
+    if(e.target.closest('.btn-openAddTask')){
+        alternarAperturaCierre();
+    }
 });
 
 /* ############################# CIERRE */
@@ -27,3 +56,5 @@ const alternarAperturaCierre = () => {
     modalAddTask.classList.toggle('active');
     return;
 }
+
+verificarSiHayTareas();
